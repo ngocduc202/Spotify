854b4b9db625189494914d402ce514e62947466f
ngocduc202 <ngocduc1542002@gmail.com>
2024-11-13 16:19:44 +0700

    create MainLayout & LeftSidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "@/pages/home/HomePage.tsx";
 import { AuthCallbackPage } from "@/pages/auth-callback/AuthCallbackPage.tsx";
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+import MainLayout from "./layout/MainLayout";
+import ChatPage from "./pages/chat/ChatPage";
 
 
 function App() {
@@ -11,11 +13,14 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<HomePage />} />
         <Route path='/sso-callback' element={<AuthenticateWithRedirectCallback
           signUpForceRedirectUrl={'auth-callback'}
         />} />
         <Route path='/auth-callback' element={<AuthCallbackPage />} />
+        <Route element={<MainLayout />}>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/chat' element={<ChatPage />} />
+        </Route>
       </Routes>
     </>
   )
